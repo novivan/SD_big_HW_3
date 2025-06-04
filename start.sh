4df642ab23c6e5e4fb406a004584666a2e49af9d
Ivan Novikov <ivannovikov@Ivans-MacBook-Pro.local>
2025-06-05 02:26:28 +0300

    добавил поямнений в start.sh

--- start.sh
+++ start.sh
@@ -3,8 +3,10 @@
 echo "Starting the application..."
 
 cd ./API_Gateway && mvn clean package 
-#nohup чтобы запустить процесс в фоновом режиме
-#> /dev/null 2>&1 & перенаправляет лог из nohup.out в мусор (чтоб не создавать лишних логов)
+# & (в конце строки) чтобы запустить процесс в фоновом режиме
+# nohup позволяет процессу продолжать работать даже после закрытия терминала
+# > /dev/null 2>&1 перенаправляет лог из nohup.out в мусор (чтоб не создавать лишних логов)
+# а также из файла с дескриптором 2 в файл с дескриптором 1 (поток stderr в stdout) (который в данном случае перенаправлен в /dev/null)
 nohup mvn exec:java -Dexec.mainClass="com.example.App" > /dev/null 2>&1 &
 cd ..
 echo "API_Gateway started successfully."
